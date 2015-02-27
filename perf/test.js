2d28c72e1776a9d5b0addbf3fd5313dc3a620c46
anvaka <anvaka@gmail.com>
2015-02-26 20:25:54 -0800

    Added performance suite

---
+++ perf/test.js
@@ -0,0 +1,18 @@
+var g = require('ngraph.generators').grid(100, 100);
+var count = 100;
+var pagerank = require('../');
+var Benchmark = require('benchmark');
+var suite = new Benchmark.Suite;
+
+suite.add('Epsilon 1e-7', function() {
+  pagerank(g, 0.85, 1e-7);
+}).add('Epsilon 1e-10', function() {
+  pagerank(g, 0.85, 1e-10);
+}).on('cycle', function(event) {
+  console.log(String(event.target));
+})
+.on('complete', function() {
+  console.log('Fastest is ' + this.filter('fastest').pluck('name'));
+})
+// run async
+.run({ 'async': true });
